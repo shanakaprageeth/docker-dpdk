1ff3d0c8cfad333a7004f544e5b65af871662ee3
Shanaka Prageeth Abeysiriwardhana <shanakaprageeth@gmail.com>
2016-11-21 10:46:39 +0900

    docker files

--- docker-ubuntu-dpdk/install-image-with-docker.sh
+++ docker-ubuntu-dpdk/install-image-with-docker.sh
@@ -3,11 +3,11 @@
 # build docker file
 sudo su
 docker build -t shanakaprageeth/ubuntu-dpdk .
-docker run --privileged  --cap-add=ALL --name ubuntu-dpdk -it  ubuntu-dpdk bash
+docker run --privileged  --cap-add=ALL -v /sys/bus/pci/devices:/sys/bus/pci/devices -v /sys/kernel/mm/hugepages:/sys/kernel/mm/hugepages -v /sys/devices/system/node:/sys/devices/system/node -v /dev:/dev --name ubuntu-dpdk -it  ubuntu-dpdk bash
 
 
 # pull from docker hub
-#docker run --privileged  --cap-add=ALL --name ubuntu-dpdk -it  shanakaprageeth/ubuntu-dpdk bash
+#docker run --privileged  --cap-add=ALL -v /sys/bus/pci/devices:/sys/bus/pci/devices -v /sys/kernel/mm/hugepages:/sys/kernel/mm/hugepages -v /sys/devices/system/node:/sys/devices/system/node -v /dev:/dev --name ubuntu-dpdk -it  shanakaprageeth/ubuntu-dpdk bash
 
 
 
