a1b9d466a7ff34ae51ef6dbdc08a97d8943f75a4
Shanaka Prageeth Abeysiriwardhana <shanakaprageeth@gmail.com>
2016-11-21 10:50:56 +0900

    ubuntu script update

---
+++ docker-ubuntu-dpdk/README.md
@@ -0,0 +1,13 @@
+# docker-dpdk
+# 
+# 
+# run install-image-with-docker.sh with administrative privillages. to create the docker container
+# you could install and use dpdk by using the installation script provided by dpdk/tools/dpdk-setup.sh
+# 
+# 
+# 
+# Thank you
+# 
+# 
+# 
+
--- docker-ubuntu-dpdk/install-image-with-docker.sh
+++ docker-ubuntu-dpdk/install-image-with-docker.sh
@@ -2,7 +2,7 @@
 
 # build docker file
 sudo su
-docker build -t shanakaprageeth/ubuntu-dpdk .
+docker build -t ubuntu-dpdk .
 docker run --privileged  --cap-add=ALL -v /sys/bus/pci/devices:/sys/bus/pci/devices -v /sys/kernel/mm/hugepages:/sys/kernel/mm/hugepages -v /sys/devices/system/node:/sys/devices/system/node -v /dev:/dev --name ubuntu-dpdk -it  ubuntu-dpdk bash
 
 
