a85ef0b42fb926ea29043c580d6d7f76c7c7dd43
Shanaka Prageeth <Shanakaprageeth@gmail.com>
2025-03-13 07:45:39 +0900

    [ci]: add docker run test with huge page assignment in docker (#2)
    Changed 
    .ci.yml
    Dockerfile

--- docker-rhel-dpdk/install-image-with-docker.sh
+++ docker-rhel-dpdk/install-image-with-docker.sh
@@ -6,17 +6,29 @@ DEBIAN_FRONTEND=noninteractive
 PROGRAM_NAME="$(basename $0)"
 BASEDIR=$(dirname $(realpath "$0"))
 
-# build docker file
-docker build -t rhel8-dpdk .
-docker run --privileged  --cap-add=ALL \
-    -v /sys/bus/pci/devices:/sys/bus/pci/devices \
-    -v /sys/kernel/mm/hugepages:/sys/kernel/mm/hugepages \
-    -v /sys/devices/system/node:/sys/devices/system/node \
-    -v /dev:/dev \
-    -v /home/$USER:/home/$USER \
-    --name rhel-dpdk -it  rhel8-dpdk bash
+# build docker file if argument is provided
+if [ "$1" == "build" ]; then
+    docker build -t rhel8-dpdk .
+    docker run --privileged  --cap-add=ALL \
+        -v /sys/bus/pci/devices:/sys/bus/pci/devices \
+        -v /sys/kernel/mm/hugepages:/sys/kernel/mm/hugepages \
+        -v /sys/devices/system/node:/sys/devices/system/node \
+        -v /dev:/dev \
+        -v /home/$USER:/home/$USER \
+        --name rhel-dpdk -it  rhel8-dpdk bash
+else
+    # pull from docker hub
+    docker run --privileged  --cap-add=ALL \
+        -v /sys/bus/pci/devices:/sys/bus/pci/devices \
+        -v /sys/kernel/mm/hugepages:/sys/kernel/mm/hugepages \
+        -v /sys/devices/system/node:/sys/devices/system/node \
+        -v /dev:/dev \
+        -v /home/$USER:/home/$USER \
+        --name rhel-dpdk -it  shanakaprageeth/rhel8-dpdk bash
+fi
 
-
+#docker exec rhel-dpdk bash -c "/root/dpdk/usertools/dpdk-hugepages.py -p 2048K --setup 2M --node 0"
+#docker exec rhel-dpdk bash -c "cat /proc/meminfo | grep HugePages"
 
 
 
